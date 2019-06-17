294249923c96ae42e6314dc97d6a5534c74459b9
Sjun <suijun1@yusys.com.cn>
2019-06-17 11:04:05 +0800

    分支开发

---
+++ cmis/src/main/java/com/yucehng/cmis/Main.java
@@ -0,0 +1,23 @@
+package com.yucehng.cmis;
+
+/**
+ * 
+ * <p>
+ * <h2>简述</h2>
+ * 		<ol>请添加简述</ol>
+ * <h2>功能描述</h2>
+ * 		<ol>请添加功能详细的描述</ol>
+ * <h2>修改历史</h2>
+ *      <ol>如有修改，添加修改历史</ol>
+ * </p>
+ * @author Sjun
+ * @Date 2019年6月17日 上午11:02:49
+ * @version 1.0
+ */
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
